f4df86d327938417b2976eab84db9ba637ff90e3
samy <udev9985@gmail.com>
2025-02-26 16:37:26 +0100

    add about page and github infos api

--- src/app/data.js
+++ src/app/data.js
@@ -96,19 +96,19 @@ export const BtnList = [
   { label: "Contact", link: "/contact", icon: "contact", newTab: false },
   {
     label: "Github",
-    link: "https://www.github.com/codebucks27",
+    link: "https://www.github.com/samyferhat",
     icon: "github",
     newTab: true,
   },
   {
     label: "LinkedIn",
-    link: "https://www.linkedin.com/in/codebucks",
+    link: "https://www.linkedin.com/in/samyferhat",
     icon: "linkedin",
     newTab: true,
   },
   {
     label: "X",
-    link: "https://www.x.com/code_bucks",
+    link: "https://www.x.com/",
     icon: "twitter",
     newTab: true,
   },
